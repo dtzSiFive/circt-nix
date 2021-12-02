b57185cab7c4612cb849299ed96b0d09a8ea587a
Will Dietz <w@wdtz.org>
2021-12-02 14:12:01 -0600

    mlir oops this is just llvm

--- default.nix
+++ default.nix
@@ -7,6 +7,15 @@ let
       "-DLLVM_ENABLE_ASSERTIONS=ON"
       "-DLLVM_ENABLE_PROJECTS=mlir"
     ];
+
+    postPatch = o.postPatch or "" + ''
+      pwd
+      ls
+      echo ------
+      ls projects
+      echo ------
+      chmod u+rw -R mlir
+    '';
   });
   llvmTest = mlir-llvm pkgs.llvmPackages_latest.llvm;
 
