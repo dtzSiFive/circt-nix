d1e1e8c8b7d334b1127254f3060356e542bb3fe9
Will Dietz <w@wdtz.org>
2021-12-03 18:25:08 -0600

    wake.nix: deps, try building

--- wake.nix
+++ wake.nix
@@ -1,8 +1,11 @@
-{ pkgs ? import <nixpkgs> { }, wake-src }:
-with pkgs;
+{ wake-src, stdenv, pkgconfig, dash, fuse, gmp, ncurses, re2, sqlite }:
 
 stdenv.mkDerivation {
   pname = "wake";
   version = wake-src.shortRev;
+  src = wake-src;
+
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ dash fuse gmp ncurses re2 sqlite ];
 }
 
