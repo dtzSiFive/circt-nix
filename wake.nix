0237b9ac08e898ce2a8261918e57ea238311ac41
Will Dietz <w@wdtz.org>
2021-12-03 18:35:35 -0600

    wake.nix: git

--- wake.nix
+++ wake.nix
@@ -1,11 +1,11 @@
-{ wake-src, stdenv, pkgconfig, dash, fuse, gmp, ncurses, re2c, re2, sqlite }:
+{ wake-src, stdenv, pkgconfig, git, dash, fuse, gmp, ncurses, re2c, re2, sqlite }:
 
 stdenv.mkDerivation {
   pname = "wake";
   version = wake-src.shortRev;
   src = wake-src;
 
-  nativeBuildInputs = [ pkgconfig re2c ];
+  nativeBuildInputs = [ pkgconfig re2c git ];
   buildInputs = [ dash fuse gmp ncurses re2 sqlite ];
 
   # install via wake?
