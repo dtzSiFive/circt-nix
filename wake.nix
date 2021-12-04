0d64287f116e8d83f1d176b6c6d1f6e39c7b9134
Will Dietz <w@wdtz.org>
2021-12-03 18:31:41 -0600

    wake.nix: re2c instead

--- wake.nix
+++ wake.nix
@@ -1,12 +1,12 @@
-{ wake-src, stdenv, pkgconfig, dash, fuse, gmp, ncurses, re2, sqlite }:
+{ wake-src, stdenv, pkgconfig, dash, fuse, gmp, ncurses, re2c, sqlite }:
 
 stdenv.mkDerivation {
   pname = "wake";
   version = wake-src.shortRev;
   src = wake-src;
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ dash fuse gmp ncurses re2 sqlite ];
+  nativeBuildInputs = [ pkgconfig re2c ];
+  buildInputs = [ dash fuse gmp ncurses sqlite ];
 
   # install via wake?
 
