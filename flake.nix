cd55f9ba09a2a838e0edf5244c341a5f0e5a615f
Will Dietz <w@wdtz.org>
2022-03-09 17:29:56 -0600

    checkpoint

--- flake.nix
+++ flake.nix
@@ -11,24 +11,32 @@
       ref = "v0.24.0";
       flake = false;
     };
-    circt-src = {
-      type = "git";
-      url = "https://github.com/llvm/circt";
-      ref = "main";
-      flake = false;
-      submodules = true;
-    };
-    #circt-src = {
-    #  type = "github";
-    #  owner = "llvm";
-    #  repo = "circt";
-    #  submodules = true;
+    #circt-git-src = {
+    #  type = "git";
+    #  url = "https://github.com/llvm/circt";
     #  ref = "main";
     #  flake = false;
+    #  submodules = true;
     #};
+    circt-src = {
+      type = "github";
+      owner = "llvm";
+      repo = "circt";
+      #submodules = true;
+      ref = "main";
+      flake = false;
+    };
+    llvm-submodule-src = {
+      type = "github";
+      owner = "llvm";
+      repo = "llvm-project";
+      #submodules = true;
+      #rev = "main";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, circt-src, wake-src }:
+  outputs = { self, nixpkgs, flake-utils, circt-src, llvm-submodule-src, wake-src }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let pkgs = nixpkgs.legacyPackages.${system}; in
@@ -40,7 +48,7 @@
             circt = import ./default.nix { inherit pkgs circt-src; };
           };
           # defaultPackage = packages.foo;
-          defaultPackage = packages.hello;
+          defaultPackage = packages.circt;
 
           #defaultPackage = import ./build.nix { inherit self pkgs; };
         }
