a250662e978a1a1fdfca9c1e494b4e2e1c6a8651
Will Dietz <w@wdtz.org>
2022-03-09 16:05:42 -0600

    notes/thoughts

--- default.nix
+++ default.nix
@@ -13,6 +13,13 @@ let
 
   # circt (source) as flake input? >:D
 
+  #-----------------
+
+  # Upstream mlir into nixpkgs
+  # - Add to git variant (?), if that helps here (easy to set git hash?)
+  # Here, override llvm source to our submodule and build MLIR from local submodule as well
+  # If nix flakes+submodules are pain, just bake llvm commit as flake input, bump manually for now
+
   mlir-llvm = llvm: llvm.overrideAttrs(o: {
     src = "${circt-src}/llvm";
     sourceRoot = "llvm/llvm";
