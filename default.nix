4c4e2ec40cc01920e47c94d5c70d2e3d0cdde288
Will Dietz <w@wdtz.org>
2022-03-14 13:13:42 -0500

    circt: check-circt

--- default.nix
+++ default.nix
@@ -97,7 +97,7 @@ let
     ];
 
     doCheck = true;
-    checkTarget = "check-all";
+    checkTarget = "check-circt";
     #cmakeFlags = [
     #  "-DMLIR_DIR=${mlir-new.dev}/lib/cmake/mlir"
     #  "-DMLIR_TABLEGEN_EXE=${mlir-new}/bin/mlir-tblgen"
