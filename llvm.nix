0c35360f9ae0c5dd913e65bf14eeef494b23b0de
Will Dietz <w@wdtz.org>
2022-03-19 17:06:31 -0500

    llvm: use patched source for all

--- llvm.nix
+++ llvm.nix
@@ -4,14 +4,8 @@ let
     cp -r ${src} "$out"
     chmod u+rw -R $out
   '' + lib.concatMapStringsSep "\n" (p: "patch -p1 -i ${p} -d $out") patches);
-  monorepoSrc = llvm-submodule-src;
-  mlir-monorepoSrc = patchsrc llvm-submodule-src [
+  monorepoSrc = patchsrc llvm-submodule-src [
     ./patches/mlir-set-CMAKE_INCLUDE_CURRENT_DIR.patch
-    #(fetchpatch {
-    #  url = "https://github.com/llvm/llvm-project/commit/03078ec20b12605fd4dfd9fe9c98a26c9d2286d7.patch";
-    #  sha256 = "sha256-fxK4gMfbKcOVhCjynKTx5R9qx+e7Y3eSAoocjTL3ewY=";
-    #  revert = true;
-    #})
   ];
   version = "git-${llvm-submodule-src.shortRev}";
   newPkgs = rec  {
@@ -27,8 +21,7 @@ let
       ln -s ${libllvm-unpatched.out} $out
     '';
     mlir = llvmPackages.mlir.override {
-      inherit libllvm version;
-      monorepoSrc = mlir-monorepoSrc;
+      inherit monorepoSrc libllvm version;
     };
     libclang = llvmPackages.libclang.override {
       inherit monorepoSrc libllvm version;
