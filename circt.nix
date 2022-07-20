1a9c89e6ba6ea6473f591c14d5cfc663951a8013
Will Dietz <w@wdtz.org>
2022-07-20 17:39:41 -0500

    circt: 1.8.0 -> 1.10.0

--- circt.nix
+++ circt.nix
@@ -14,7 +14,7 @@
 # TODO: or-tools, needs cmake bits maybe?
 stdenv.mkDerivation {
   pname = "circt";
-  version = "1.8.0-git-${circt-src.shortRev}";
+  version = "1.10.0-git-${circt-src.shortRev}";
   nativeBuildInputs = [ cmake python3 ninja doxygen graphviz-nox ];
   buildInputs = [ mlir libllvm capnproto verilator ];
   src = circt-src;
