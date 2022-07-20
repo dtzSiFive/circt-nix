9e2f1e0dc878dd6949fb7bf70ed50c4e06ed3e46
Will Dietz <w@wdtz.org>
2022-07-20 17:38:40 -0500

    circt: enable assertions

--- circt.nix
+++ circt.nix
@@ -7,6 +7,7 @@
 , ninja
 , doxygen
 , graphviz-nox
+, assertions ? true
 }:
 
 
@@ -33,6 +34,7 @@ stdenv.mkDerivation {
     "-DCapnProto_DIR=${capnproto}/lib/cmake/CapnProto"
     "-DLLVM_BUILD_MAIN_SRC_DIR=${llvmUtilsSrc}"
     "-DCIRCT_INCLUDE_DOCS=ON"
+    "-DLLVM_ENABLE_ASSERTIONS=${if assertions then "ON" else "OFF"}"
   ];
 
   postBuild = "ninja doxygen-circt circt-doc";
