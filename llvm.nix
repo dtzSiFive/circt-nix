715ff0a06d86f1d1f39d35ba7ce2a439bf821ae9
Will Dietz <w@wdtz.org>
2022-08-16 20:48:05 -0500

    llvm.nix: drop kludgery no longer needed, woohoo!

--- llvm.nix
+++ llvm.nix
@@ -9,23 +9,11 @@ let
   monorepoSrc = patchsrc llvm-submodule-src [
   ];
   version = "git-${llvm-submodule-src.shortRev}";
-  newPkgs = rec  {
+  newPkgs = rec {
     libllvm = (llvmPackages.libllvm.override { inherit monorepoSrc version; }).overrideAttrs(o: {
       cmakeFlags = o.cmakeFlags or [] ++ [ "-DLLVM_ENABLE_ASSERTIONS=ON" ];
     });
-    # Patch up installed cmake files: projects using LLVM cannot and should not have to install their binaries
-    # into the same prefix as LLVM was built with.
-    # TODO: Fixed upstream?? 07b749800c5cd4105d49ab46be5f0a2079dd709a
-    ## libllvm = runCommand "llvm-cmake-patched" { outputs = [ "out" "lib" "dev" ]; } ''
-    ##   mkdir -p $dev/lib
-    ##   cp -r ${libllvm-unpatched.dev}/lib/cmake $dev/lib
-    ##   for x in $dev/lib/cmake/llvm/{TableGen,AddLLVM}.cmake; do
-    ##     substituteInPlace "$x" --replace 'DESTINATION ''${LLVM_TOOLS_INSTALL_DIR}' 'DESTINATION ''${CMAKE_INSTALL_BINDIR}'
-    ##   done
-    ##   ln -s ${libllvm-unpatched.dev}/{bin,include,nix-support} $dev/
-    ##   ln -s ${libllvm-unpatched.lib} $lib
-    ##   ln -s ${libllvm-unpatched.out} $out
-    ## '';
+
     mlir = (llvmPackages.mlir.override {
       inherit monorepoSrc libllvm version;
     }).overrideAttrs (o: {
