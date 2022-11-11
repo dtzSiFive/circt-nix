307db5a27dfbe790d64566d87098d73acd752705
Will Dietz <will.dietz@sifive.com>
2022-11-11 13:24:07 -0600

    Add workaround to fix unittest discovery/hookup.

--- circt.nix
+++ circt.nix
@@ -38,6 +38,7 @@ in stdenv.mkDerivation {
 
   patches = [
     ./patches/circt-mlir-tblgen-path.patch
+    ./patches/circt-unittest-third-party.patch
   ];
   postPatch = ''
     substituteInPlace CMakeLists.txt --replace @MLIR_TABLEGEN_EXE@ "${mlir}/bin/mlir-tblgen"
