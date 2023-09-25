fd3becbecd6d4b9c5039610f2736fc42de72dcd1
Will Dietz <will.dietz@sifive.com>
2023-09-25 09:34:26 -0500

    slang: 3.0 -> 4.0
    This expression tracks git, but version tag is expected to reflect
    latest release tag (usually).
    
    https://github.com/MikePopoloski/slang/releases/tag/v4.0

--- slang.nix
+++ slang.nix
@@ -11,7 +11,7 @@ let
       date = builtins.substring 0 8 (src.lastModifiedDate or src.lastModified or "19700101");
     in
       "g${date}_${getRev src}";
-  tag = "3.0";
+  tag = "4.0";
   version = "${tag}${mkVer slang-src}";
 
   fmt_src = fetchFromGitHub {
