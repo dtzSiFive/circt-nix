d966449fd0af258a0993c72bfddcb5533c95cf6f
Will Dietz <will.dietz@sifive.com>
2024-01-17 08:18:42 -0600

    shell: -pycapnp, marked broken re:capnp 1.0

--- shell.nix
+++ shell.nix
@@ -20,7 +20,7 @@ let
   theStdenv = overrideCC llvmPkgs.stdenv (llvmPkgs.stdenv.cc.override {
     inherit (llvmPkgs) bintools;
   });
-  python = python3.withPackages (ps: [ ps.psutil ps.pycapnp ps.numpy ps.pybind11 ps.pyyaml ]);
+  python = python3.withPackages (ps: [ ps.psutil /* ps.pycapnp */ /* BROKEN re:capnp 1.0 */ ps.numpy ps.pybind11 ps.pyyaml ]);
 in
 (mkShell.override { stdenv = theStdenv; }) {
   nativeBuildInputs = [
