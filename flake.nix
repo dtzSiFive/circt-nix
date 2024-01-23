2c78849b7301afbc0ae44952b71fcd75a2c46eda
Will Dietz <will.dietz@sifive.com>
2024-01-23 15:35:16 -0600

    llvm-submodule-src: bump before update

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       owner = "llvm";
       repo = "llvm-project";
       # From circt submodule
-      rev = "c532033e0fe7aa7f88127fcee72f67e1af399136";
+      rev = "1f6f19935c1b4512190f1bc94ebf94f3d2b69911";
       flake = false;
     };
     slang-src.url = "github:MikePopoloski/slang";
