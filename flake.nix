c9a0693d5b30530d9fefdeffd8df0b667cd55403
Will Dietz <will.dietz@sifive.com>
2025-09-03 13:23:13 -0500

    flake: bump nixpkgs to pickup tweaked gnu-install-dirs patch

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/9af629d964124ab36b339699513a0135f1acd7f2";
+    nixpkgs.url = "github:NixOS/nixpkgs/df976bd993985c1c52b31ecd24e8378eea2f1ece";
     circt-src.url = "github:llvm/circt";
     circt-src.flake = false;
     llvm-submodule-src = {
